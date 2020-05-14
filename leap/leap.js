28f2c433f7f975bb2165520e2ab41e593e652284
Stephen <stephen.c.weiss@gmail.com>
2020-05-13 19:02:56 -0500

    style: inline it

--- leap/leap.js
+++ leap/leap.js
@@ -1,3 +1,2 @@
-export const isLeap = (year) => {
-  return  year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)
-};
+export const isLeap = (year) =>
+  year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
